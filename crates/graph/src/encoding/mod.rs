7f060420c09413acb6ee3ec0b60d04d28753e171
kracunovic92 <lazar.kracunovic92@gmail.com>
2026-03-10 13:29:48 +0100

    [remove] some stuff

--- crates/graph/src/encoding/mod.rs
+++
@@ -1,9 +0,0 @@
-//! Encodings from graphs into algebraic / boolean constraint systems.
-//!
-//! Currently supported:
-//! - k-coloring encoding (boolean polynomial system)
-
-/// Module for NP coloring problem
-pub mod coloring;
-
-pub use coloring::{BoolPolyBuilder, ColoringEncoding, VarIndex, build_k_coloring_system};
